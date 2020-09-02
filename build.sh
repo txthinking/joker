0f2f0a71db3fa9f737673ae5738576ed8f6d48af
txthinking <cloud@txthinking.com>
2020-09-02 07:57:16 +0000

    buildsh

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "./build.sh version"
+    exit
+fi
+
+mkdir _
+
+make clean
+make
+mv joker _/joker_linux_amd64
+
+make clean
+docker run --rm -v $(pwd):/workdir -e CROSS_TRIPLE=aarch64-linux-gnu multiarch/crossbuild make
+mv joker _/joker_linux_arm64
+
+make clean
+docker run -it --rm -v $(pwd):/workdir -e CROSS_TRIPLE=x86_64-apple-darwin multiarch/crossbuild make
+mv joker _/joker_darwin_amd64
+
+nami release github.com/txthinking/joker $1 _
+
+rm -rf _
